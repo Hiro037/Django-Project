93e45e700311f53d17f38326c92916284c1b4228
Said Magomedov <saidmagomedov037@gmail.com>
2025-06-24 22:01:12 +0500

    Приложение mailings ( mailings ) Создано приложение для рассылок Приложение подключено к проекту В приложении инициализированы все базовые модели Выполнены миграции

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
-from django.conf.global_settings import AUTH_USER_MODEL, SECRET_KEY
+from django.conf.global_settings import SECRET_KEY
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "users",
+    "mailings",
 ]
 
 MIDDLEWARE = [
---
+++ mailings/models.py
@@ -0,0 +1,94 @@
+from django.db import models
+
+# from users.models import User
+
+
+class Recipient(models.Model):
+    # Получатель рассылки
+    email = (
+        models.EmailField(
+            unique=True,
+            verbose_name="email",
+            help_text="Электронная почта получателя рассылки",
+        ),
+    )
+    full_name = (
+        models.CharField(verbose_name="ФИО", help_text="ФИО получателя рассылки"),
+    )
+    comment = (models.TextField(blank=True, null=True, verbose_name="Комментарий"),)
+    owner = models.ForeignKey(
+        to="users.User", on_delete=models.SET_NULL, blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Получатель рассылки (клиент)"
+        verbose_name_plural = "Получатели рассылки (клиенты)"
+        permissions = [
+            ("can_view_all_recipients", "Может просматривать всех получателей рассылок")
+        ]
+
+
+class Message(models.Model):
+    # Сообщение
+    subject = models.CharField(verbose_name="Тема письма")
+    body = (models.TextField(verbose_name="Тело письма"),)
+    owner = models.ForeignKey(
+        to="users.User", on_delete=models.SET_NULL, blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        verbose_name = "Сообщение"
+        verbose_name_plural = "Сообщения"
+        permissions = [("can_view_all_messages", "Может просматривать все сообщения")]
+
+
+class Mailing(models.Model):
+    # Рассылка
+    start_time = models.DateTimeField(verbose_name="Начало рассылки")
+    end_time = models.DateTimeField(verbose_name="Окончание рассылки")
+    status = models.CharField(
+        choices=[
+            ("CREATED", "Создана"),
+            ("FINISHED", "Завершена"),
+            ("STARTED", "Запущена"),
+        ],
+        verbose_name="Статус рассылки",
+    )
+    message = models.ForeignKey(to="Message", on_delete=models.CASCADE)
+    recipients = models.ManyToManyField(
+        to="Recipient", verbose_name="Получатель рассылки"
+    )
+    owner = models.ForeignKey(
+        to="users.User", on_delete=models.SET_NULL, blank=True, null=True
+    )
+
+    class Meta:
+        verbose_name = "Рассылка"
+        verbose_name_plural = "Рассылки"
+        permissions = [
+            ("can_view_all_mailings", "Может просматривать все рассылки"),
+            ("can_disable_mailings", "Может отключать чужие рассылки"),
+        ]
+
+
+class MailingAttempt(models.Model):
+    # Попытка рассылки
+    timestamp = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        choices=[("SUCCESS", "Успешно"), ("FAILURE", "Не успешно")],
+        verbose_name="Статус попытки рассылки",
+    )
+    server_response = models.TextField(verbose_name="Ответ сервера")
+    mailing = models.ForeignKey(
+        to="Mailing", on_delete=models.SET_NULL, blank=True, null=True
+    )
+
+    class Meta:
+        verbose_name = "Попытка рассылки"
+        verbose_name_plural = "Попытки рассылки"
