8957f0b2c79ee36023bd1ea1f71d80f7fe5b8cf3
Said Magomedov <saidmagomedov037@gmail.com>
2025-06-26 18:38:27 +0500

    Исправление ошибок

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
-from django.conf.global_settings import SECRET_KEY
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -25,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv(SECRET_KEY)
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
